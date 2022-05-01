b23393b7f7991174360b93a1af44bbe158f66018
yuadh <1912546219@qq.com>
2022-05-01 22:18:12 +0800

    init

---
+++ docs/.vuepress/public/resource/webNav/dataArray.js
@@ -0,0 +1,20 @@
+
+export default{
+  // 个人网站导航数组
+  myWebItems:[
+    {
+      item_title:'yuadhのstudy_time',
+      item_desc:'个人编程主页',
+      item_icon_href:'#icon-mdn',
+      item_a_href:'https://www.yuadh.com',
+      item_a_title:'yuadh'
+    },
+    {
+      item_title:'yuadhのstudy_time',
+      item_desc:'个人编程主页',
+      item_icon_href:'#icon-mdn',
+      item_a_href:'https://www.yuadh.com',
+      item_a_title:'yuadh'
+    },
+  ]
+}
