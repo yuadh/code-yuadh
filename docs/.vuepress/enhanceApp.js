603e9ea0b46418417026aa9b2861699d4be3b926
yuadh <1912546219@qq.com>
2022-05-08 20:58:11 +0800

    week

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -1,6 +1,6 @@
 // import vue from 'vue/dist/vue.esm.browser'
-// import Element from 'element-ui';
-// import 'element-ui/lib/theme-chalk/index.css';
+import Element from 'element-ui/lib/index';
+import 'element-ui/lib/theme-chalk/index.css';
 export default ({
   Vue, // VuePress 正在使用的 Vue 构造函数
   options, // 附加到根实例的一些选项
@@ -8,5 +8,5 @@ export default ({
   siteData // 站点元数据
 }) => {
   // window.Vue = vue // 使页面中可以使用Vue构造函数 （使页面中的vue demo生效）
-  // Vue.use(Element);
+  Vue.use(Element);
 }
