7b781378ff0f41f639a0f71bdfe120bee3580f50
yuadh <1912546219@qq.com>
2022-05-09 08:31:51 +0800

    week

--- docs/.vuepress/components/web/dat/allWords.js
+++ docs/.vuepress/components/web/dat/allWords.js
@@ -24,3 +24,30 @@ export const day01 = [
         'rarely', 'conceal', 'calculate', 'approach', 'resolve'
     ],
 ]
+
+export const day02 = [
+    ['accelerate', 'indispensable', 'abuse', 'evaluate', 'primarily',
+        'grave', 'architect', 'conflict', 'confuse', 'sheer',
+        'absorb', 'prompt', 'supply', 'deny', 'superficial'
+    ],
+    ['substitute', 'recruit', 'maintain', 'contain', 'obtain',
+        'compromise', 'privilege', 'demonstrate', 'claim', 'challenge',
+        'condemn', 'symbol', 'sample', 'advantage', 'assign'
+    ],
+    ['imitate', 'controversy', 'contradiction', 'pressure', 'consistent',
+        'constant', 'instant', 'disguise', 'fundamental', 'concern',
+        'reinforce', 'propose', 'proposal', 'dispose', 'expose'
+    ],
+    ['compose', 'oppose', 'amateur', 'tough', 'gross',
+        'unexpected', 'reveal', 'essential', 'enhance', 'dismiss',
+        'conventional', 'enthusiasm', 'abundant', 'expand', 'advocate'
+    ],
+    ['tradition', 'refine', 'harsh', 'idle', 'contemporary',
+        'consume', 'presume', 'assume', 'resume', 'pursue',
+        'afford', 'range', 'refer', 'deceive', 'conquer'
+    ],
+    ['queue', 'authority', 'manual', 'convict', 'evil',
+        'confirm', 'balance', 'neglect', 'justify', 'prejudice',
+        'rarely', 'conceal', 'calculate', 'approach', 'resolve'
+    ],
+]
